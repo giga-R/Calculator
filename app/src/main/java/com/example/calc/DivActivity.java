e0d89b1232c2e39c2beacbde6c489007f0f744cb
giga-R <rafiamjad12@gmail.com>
2024-12-17 00:39:19 +0530

    Calculator created

---
+++ app/src/main/java/com/example/calc/DivActivity.java
@@ -0,0 +1,51 @@
+package com.example.calc;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class DivActivity extends AppCompatActivity {
+    Button dbt1,abt2;
+    EditText et1,et2;
+    int num1,num2,sum;
+    String getN1,getN2,result;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_div);
+        dbt1=(Button) findViewById(R.id.dmenu);
+        dbt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ob6=new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(ob6);
+            }
+        });
+        abt2=(Button) findViewById(R.id.divopr);
+        et1=(EditText)findViewById(R.id.dn1)  ;
+        et2=(EditText)findViewById(R.id.dn2)  ;
+        abt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getN1=et1.getText().toString();
+                getN2=et2.getText().toString();
+                num1=Integer.parseInt(getN1);
+                num2=Integer.parseInt(getN2);
+                sum=num1/num2;
+                result=String.valueOf(sum);
+                System.out.println(result);
+                Toast.makeText(getApplicationContext(),result,Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/calc/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.calc;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1,b2,b3,b4;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        b1=(Button) findViewById(R.id.addb);
+        b2=(Button) findViewById(R.id.subb);
+        b3=(Button) findViewById(R.id.mulb);
+        b4=(Button) findViewById(R.id.divb);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ob1=new Intent(getApplicationContext(), AddActivity.class);
+                startActivity(ob1);
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ob3=new Intent(getApplicationContext(), SubActivity.class);
+                startActivity(ob3);
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ob5=new Intent(getApplicationContext(), MulActivity.class);
+                startActivity(ob5);
+            }
+        });
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ob7=new Intent(getApplicationContext(), DivActivity.class);
+                startActivity(ob7);
+            }
+        });
+
+    }
+}
